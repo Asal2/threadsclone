0833875ba9aab29faaa860c354b832a0b8395015
Ashal Sharma <ashalsharma68@gmail.com>
2023-12-29 02:18:01 -0600

    Testing git

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,4 +1,4 @@
-"use server"
+"use server" 
 import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
@@ -33,4 +33,32 @@ export async function createThread({
         throw new Error (`Error created thread: ${error.message }`)
     }
     
+}
+
+export async function fetchPosts(pageNumber=1, pageSize=20){
+    connectToDB()
+
+    const skipAmount = (pageNumber - 1) * pageSize
+    const postsQuery = Thread.find({parentId: {$in: [null, undefined]}})
+        .sort({createdAt: 'desc'})
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({path: 'author', model: User})
+        .populate({
+            path: 'children',
+            populate:{
+                path: 'author',
+                model:User,
+                select:"_id name parentId image"
+            }
+        })
+
+        const totalPostsCount = await Thread.countDocuments({parentId: {$in: [null, undefined]}})
+        const posts = await postsQuery.exec();
+
+        const isNext = totalPostsCount > skipAmount + posts.length;
+        return {posts, isNext}
+
+
+        
 }
